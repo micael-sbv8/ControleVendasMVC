5051cb65dc8652621518aa2b4ab8539bbc4a8599
micael batista <micaelsbv8@gmail.com>
2022-01-23 13:13:08 -0300

    Model-controller-view - Department - scaffolding

---
+++ VendasControle/Data/VendasControleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VendasControle.Models;
+
+namespace VendasControle.Data
+{
+    public class VendasControleContext : DbContext
+    {
+        public VendasControleContext (DbContextOptions<VendasControleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VendasControle.Models.Department> Department { get; set; }
+    }
+}

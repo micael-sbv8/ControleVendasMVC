198f1e4248b321f4f66aeab80d4f2ea41fb769cf
micael batista <micaelsbv8@gmail.com>
2022-01-31 19:52:54 -0300

    Seller details

--- VendasControle/Services/SellerService.cs
+++ VendasControle/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using VendasControle.Data;
 using VendasControle.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace VendasControle.Services
 {
@@ -25,6 +26,18 @@ namespace VendasControle.Services
         {
            
             _context.Add(obj);
+            _context.SaveChanges();
+        }
+
+        public Seller FindById(int id)
+        {
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Seller.Find(id);
+            _context.Seller.Remove(obj);
             _context.SaveChanges();
         }
     }
